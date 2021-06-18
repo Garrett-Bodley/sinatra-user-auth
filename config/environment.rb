97610c92efe1a9ba453ecb1fa064f4a4f2e95889
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-06-18 15:46:44 -0400

    Archive Flatiron code.

--- config/environment.rb
+++ config/environment.rb
@@ -1,4 +1,5 @@
 require 'bundler/setup'
+require 'pry'
 Bundler.require
 
 ENV['SINATRA_ENV'] ||= "development"
@@ -7,6 +8,7 @@ ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
   :database => "db/user_auth#{ENV['SINATRA_ENV']}.sqlite"
 )
+ActiveRecord::Base.logger.level = 1 
 
 Dir[File.join(File.dirname(__FILE__), "../app/models", "*.rb")].each {|f| require f}
 Dir[File.join(File.dirname(__FILE__), "../app/controllers", "*.rb")].each {|f| require f}
